da22e696efb8ee360553946077bc00125566cf75
Juiko <pulga.p.29@gmail.com>
2013-06-13 17:01:06 -0400

    Deleted (dmenu-build-list)

--- dmenu-wrapper.lisp
+++ dmenu-wrapper.lisp
@@ -22,13 +22,13 @@
           (when *dmenu-foreground-color* (format nil "-nf ~A" *dmenu-foreground-color*))
           (when *dmenu-selected-background-color* (format nil "-sb ~A" *dmenu-selected-background-color*))))
 
-(defun dmenu-build-list (item-list)
-  (if item-list (format nil "\"~{~A\\n~}\"" item-list) ""))
+;; (defun dmenu-build-list (item-list)
+;;   (if item-list (format nil "\"~{~A\\n~}\"" item-list) ""))
 
-(defun dmenu (&key item-list prompt vertical-lines) 
+(defun dmenu (&key item-list prompt vertical-lines)
   (let* ((cmd (format nil
                       "printf ~A | dmenu~A ~@[-p \"~A\"~] ~@[-l \"~A\"~]"
-                      (dmenu-build-list item-list)
+                      (if item-list (format nil "\"~{~A\\n~}\"" item-list) "")
                       (dmenu-build-cmd-options)
                       prompt
                       vertical-lines))
@@ -39,33 +39,30 @@
 ;; Examples of what can be done with (dmenu) ....
 
 (defcommand dmenu-call-command () ()
-  "Uses dmenu to call a Stumpwm command"
-  (let ((selection (dmenu :item-list (all-commands) :prompt "Commands:")))
-    (when selection (run-commands selection))))
+            "Uses dmenu to call a Stumpwm command"
+            (let ((selection (dmenu :item-list (all-commands) :prompt "Commands:")))
+              (when selection (run-commands selection))))
 
 (defcommand dmenu-windowlist () ()
-  "Uses dmenu to change the visible window"
-  (labels ((get-window (window-name)
-             (first (remove-if #'null
-                               (mapcar
-                                #'(lambda (x) (when (equal window-name (window-title x)) x))
-                                (all-windows))))))
-    (let* ((open-windows (mapcar #'window-name (all-windows)))
-           (num-of-windows (length open-windows))
-           (win-name (dmenu
-                     :item-list open-windows
-                     :prompt "Choose a window:"
-                     :vertical-lines (if (> num-of-windows *dmenu-max-vertical-lines*)
-                                         *dmenu-max-vertical-lines*
-                                         num-of-windows))))
-      (when win-name (group-focus-window (current-group) (get-window win-name))))))
+            "Uses dmenu to change the visible window"
+            (labels ((get-window (window-name)
+                       (first (remove-if #'null
+                                         (mapcar
+                                          #'(lambda (x) (when (equal window-name (window-title x)) x))
+                                          (all-windows))))))
+              (let* ((open-windows (mapcar #'window-name (all-windows)))
+                     (num-of-windows (length open-windows))
+                     (win-name (dmenu
+                                :item-list open-windows
+                                :prompt "Choose a window:"
+                                :vertical-lines (if (> num-of-windows *dmenu-max-vertical-lines*)
+                                                    *dmenu-max-vertical-lines*
+                                                    num-of-windows))))
+                (when win-name (group-focus-window (current-group) (get-window win-name))))))
 
 (defcommand dmenu-kill-proc () ()
-  (run-shell-command (format nil "pkill \"$(pidof ~A)\"" (dmenu :prompt "pkill:"))))
+            (run-shell-command (format nil "pkill \"$(pidof ~A)\"" (dmenu :prompt "pkill:"))))
 
 (defcommand dmenu-run () ()
-  "Just a simple wrapper to call dmenu_run from lisp"
-  (run-shell-command (format nil "dmenu_run ~A" (dmenu-build-cmd-options))))
-    
-  
-
+            "Just a simple wrapper to call dmenu_run from lisp"
+            (run-shell-command (format nil "dmenu_run ~A" (dmenu-build-cmd-options))))
