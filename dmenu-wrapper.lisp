d1ba5513422d6c98f0e29bc4568aba5f74d202e4
Juiko <pulga.p.29@gmail.com>
2013-06-26 00:07:04 -0400

    Tried to clean up (dmenu-windowist) a bit

--- dmenu-wrapper.lisp
+++ dmenu-wrapper.lisp
@@ -22,15 +22,11 @@
           (when *dmenu-foreground-color* (format nil "-nf ~A" *dmenu-foreground-color*))
           (when *dmenu-selected-background-color* (format nil "-sb ~A" *dmenu-selected-background-color*))))
 
-;; (defun dmenu-build-list (item-list)
-;;   (if item-list (format nil "\"~{~A\\n~}\"" item-list) ""))
-
 (defun dmenu (&key item-list prompt vertical-lines (cmd-options (dmenu-build-cmd-options)))
   (let* ((cmd (format nil
                       "printf ~A | dmenu~A ~@[-p \"~A\"~] ~@[-l \"~A\"~]"
                       (if item-list (format nil "\"~{~A\\n~}\"" item-list) "")
                       cmd-options
-                       ;; (dmenu-build-cmd-options)
                       prompt
                       vertical-lines))
          (selection (run-shell-command cmd t)))
@@ -40,9 +36,9 @@
 ;; Examples of what can be done with (dmenu) ....
 
 (defcommand dmenu-call-command () ()
-            "Uses dmenu to call a Stumpwm command"
-            (let ((selection (dmenu :item-list (all-commands) :prompt "Commands:")))
-              (when selection (run-commands selection))))
+  "Uses dmenu to call a Stumpwm command"
+  (let ((selection (dmenu :item-list (all-commands) :prompt "Commands:")))
+    (when selection (run-commands selection))))
 
 (defcommand dmenu-eval-lisp () ()
   "Uses dmenu to eval a Lisp expression"
@@ -50,22 +46,22 @@
     (when selection (eval (read-from-string selection)))))
 
 (defcommand dmenu-windowlist () ()
-            "Uses dmenu to change the visible window"
-            (labels ((get-window (window-name)
-                       (first (remove-if #'null
-                                         (mapcar
-                                          #'(lambda (x) (when (equal window-name (window-title x)) x))
-                                          (all-windows))))))
-              (let* ((open-windows (mapcar #'window-name (all-windows)))
-                     (num-of-windows (length open-windows))
-                     (win-name (dmenu
-                                :item-list open-windows
-                                :prompt "Choose a window:"
-                                :vertical-lines (if (> num-of-windows *dmenu-max-vertical-lines*)
-                                                    *dmenu-max-vertical-lines*
-                                                    num-of-windows))))
-                (when win-name (group-focus-window (current-group) (get-window win-name))))))
+  "Uses dmenu to change the visible window"
+  (labels ((get-window (window-name)
+             (loop for w in (all-windows) do
+                  (when (equal (window-title w) window-name) (return w)))))
+
+    (let* ((open-windows (mapcar #'window-name (all-windows)))
+           (num-of-windows (length open-windows))
+           (win-name (dmenu
+                      :item-list open-windows
+                      :prompt "Choose a window:"
+                      :vertical-lines (if (> num-of-windows *dmenu-max-vertical-lines*)
+                                          *dmenu-max-vertical-lines*
+                                          num-of-windows))))
+
+      (when win-name (group-focus-window (current-group) (get-window win-name))))))
 
 (defcommand dmenu-run () ()
-            "Just a simple wrapper to call dmenu_run from lisp"
-            (run-shell-command (format nil "dmenu_run ~A" (dmenu-build-cmd-options))))
+  "Just a simple wrapper to call dmenu_run from lisp"
+  (run-shell-command (format nil "dmenu_run ~A -p Run: " (dmenu-build-cmd-options))))
